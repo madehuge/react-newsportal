7383613173be50eb1d61fde1933ed7495113bf6c
Manish Kumar Jangir <manishjangir027@gmail.com>
2025-03-20 18:00:49 +0530

    HOME JS Updated

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Slider from "../Components/Slider";
 import Newsbox from "../Components/Newsbox";
-import { Container } from "react-bootstrap";
+//import { Container } from "react-bootstrap";
 
 export default function Home() {
   return (
